8c2bc5df7c343a3b2705ac07291999881b9762c7
michaelrupertdev <mbrupert@iastate.edu>
2016-01-19 15:42:23 -0600

    - Added verbose and overwrite options - updated readme with usage

--- README.md
+++ README.md
@@ -4,6 +4,23 @@ Tools
 
 Script for crawling a given directory and converting all .mobi files to .epub files using Calibre's ebook-convert utility.
 
-Setup: Install calibre (sudo apt-get install calibre on ubuntu)
+**Setup**
+        1. Install calibre (sudo apt-get install calibre on ubuntu, download from website on mac)
+    2. If you are on mac run the following command to add cli tools to your path.
+       'export PATH=$PATH:/Applications/calibre.app/Contents/MacOS'
+    3. pip install -r requirements_dev.txt
 
-Usage: python mobi2epub.py [desired root directory] 
+
+**Usage**
+
+usage: mobi2epub.py [-h] [--dir [DIR]] [--overwrite] [--verbose]
+
+optional arguments:
+
+  -h, --help   show this help message and exit  
+
+  --dir [DIR]  Dir to start the search for MOBI files
+
+  --overwrite  Overwrite existing epub files? Defaults to false.
+
+  --verbose    Run with full output from ebook-convert
--- mobi2epub.py
+++ mobi2epub.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import logging
 import os
 import sys
 
@@ -7,22 +8,35 @@ from subprocess import call
 
 parser = ArgumentParser()
 
-parser.add_argument("start", default=".", nargs="?",
-                    help="Dir to start the search for MOBI files")
+parser.add_argument("--dir", default=".", nargs="?",
+                    help="Dir to start the search for MOBI files, \
+                    defaults to current directory")
+parser.add_argument("--overwrite", default=False, action="store_true",
+                    help="Overwrite existing epub files? Defaults to false.")
+parser.add_argument("--verbose", default=False, action="store_true",
+                    help="Run with full output from ebook-convert")
 args = parser.parse_args()
 
-def batch_convert(start_dir):
+def batch_convert(start_dir, overwrite, verbose):
     for root, dirs, files in os.walk(start_dir):
         for file in files:
             if file.endswith('.mobi'):
                 epub_path = root + '/' + file.replace('.mobi', '.epub')
                 file_path = root + '/' + file
-                ret = call(["ebook-convert", file_path, epub_path],
-                            stdout=open(os.devnull, 'wb'))
-                if ret != 0:
-                    print("Conversion of " + file + " failed, stopping....")
-                    exit()
-
-                print(file + " converted successfully")
-
-batch_convert(args.start)
+                if(overwrite or (not os.path.exists(epub_path))):
+                    try:
+                        if verbose: ret = call(["ebook-convert",
+                                                file_path, epub_path])
+                        else:
+                            ret = call(["ebook-convert", file_path, epub_path],
+                                    stdout=open(os.devnull, 'wb'),
+                                    stderr=open(os.devnull, 'wb'))
+                    except Exception as e:
+                        logging.warning(e)
+                    if ret != 0:
+                        print("Conversion of " + file + " failed")
+                        continue
+                    print(file + " converted successfully")
+                else:
+                    print(file + " skipped, epub already exists.")
+batch_convert(args.dir, args.overwrite, args.verbose)
