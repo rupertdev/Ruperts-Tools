15ed4aaddd992a4ea1edfd1639d0b344ff054c43
Michael Rupert <mbrupert@iastate.edu>
2016-05-13 18:43:10 -0500

    Made script able to convert any type to any type

--- batchconvert.py
+++ batchconvert.py
@@ -9,34 +9,43 @@ from subprocess import call
 parser = ArgumentParser()
 
 parser.add_argument("--dir", default=".", nargs="?",
-                    help="Dir to start the search for MOBI files, \
+                    help="Dir to start the search for eBook files, \
                     defaults to current directory")
 parser.add_argument("--overwrite", default=False, action="store_true",
                     help="Overwrite existing epub files? Defaults to false.")
 parser.add_argument("--verbose", default=False, action="store_true",
                     help="Run with full output from ebook-convert")
+parser.add_argument("--input", default="epub", nargs="?",
+                    help="File format to convert.")
+parser.add_argument("--out", default='mobi', nargs="?",
+                    help="File format to output.")
 args = parser.parse_args()
 
-def batch_convert(start_dir, overwrite, verbose):
+def batch_convert(start_dir, overwrite, verbose, type_in, type_out):
+    print "Looking for files.."
     for root, dirs, files in os.walk(start_dir):
         for file in files:
-            if file.endswith('.mobi'):
-                epub_path = root + '/' + file.replace('.mobi', '.epub')
+            if file.endswith(type_in):
                 file_path = root + '/' + file
-                if(overwrite or (not os.path.exists(epub_path))):
+                out_path = file_path.replace(file.split('.')[1], type_out)
+                if(overwrite or (not os.path.exists(out_path))):
                     try:
                         if verbose: ret = call(["ebook-convert",
-                                                file_path, epub_path])
+                                                file_path, out_path])
                         else:
-                            ret = call(["ebook-convert", file_path, epub_path],
+                            ret = call(["ebook-convert", file_path, out_path],
                                     stdout=open(os.devnull, 'wb'),
                                     stderr=open(os.devnull, 'wb'))
+                    except ValueError as ve:
+                        logging.error(ve)
                     except Exception as e:
                         logging.warning(e)
                     if ret != 0:
-                        print("Conversion of " + file + " failed")
+                        print("Conversion of " + file + " failed, "
+                                "run with --verbose flag to find out why.")
                         continue
                     print(file + " converted successfully")
                 else:
-                    print(file + " skipped, epub already exists.")
-batch_convert(args.dir, args.overwrite, args.verbose)
+                    print(file + " skipped, " + type_out + "  already exists.")
+
+batch_convert(args.dir, args.overwrite, args.verbose, args.input, args.out)
